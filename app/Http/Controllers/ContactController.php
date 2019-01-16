6666c849604bf054df48e8ccf0ddd94fa001ec54
Eric Davis <davis.25811@gmail.com>
2019-01-15 21:54:39 -0500

    Add form validation + controller for contact submission

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\FlashMessage;
+use App\Mail\ContactFormSubmissionToAdmin;
+use App\Http\Requests\ContactSubmissionRequest;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactSubmissionRequest $request)
+    {
+        $emails = explode(',', env('ADMIN_EMAILS'));
+        foreach($emails as $email) {
+            Mail::to($email)->send(new ContactFormSubmissionToAdmin($request->name, $request->phone, $request->date, $request->description));
+        }
+
+        FlashMessage::success('Your message has been submitted! We will contact you about your event in the next 48-72 hours.');
+        return redirect()->route('home', '#contact-form');
+    }
+
+}
