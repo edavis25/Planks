152934426a8b8664c557b19db6042c456faf1a6d
Eric Davis <davis.25811@gmail.com>
2019-01-15 20:55:10 -0500

    Add query scopes to Category model

--- app/Category.php
+++ app/Category.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
@@ -11,17 +12,49 @@ class Category extends Model
     *
     * @var array
     */
-    protected $fillable = ['name', 'details'];
+    protected $fillable = ['name', 'details', 'type'];
 
+    /**
+     * Relationship: Beers
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function beers()
     {
         return $this->hasMany('App\Beer');
     }
 
+    /**
+     * Relationship: Dishes
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function dishes()
     {
         return $this->hasMany('App\Dish');
     }
 
+    /**
+     * Scope: Food
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeFood(Builder $query)
+    {
+        return $query->where('type', 'food')
+            ->whereHas('dishes');
+    }
 
+    /**
+     * Scope: Drink
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeBeer(Builder $query)
+    {
+        return $query->where('type', 'drink')
+            ->whereHas('beers');
+    }
 }
