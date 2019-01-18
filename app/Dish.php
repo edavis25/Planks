9cbbf70989f70486ea5a2ba62039c6a77e024843
Eric Davis <davis.25811@gmail.com>
2019-01-17 23:14:56 -0500

    Add Image relationship to Dish model

--- app/Dish.php
+++ app/Dish.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Dish extends Model
 {
@@ -22,6 +23,50 @@ class Dish extends Model
     public function category()
     {
         return $this->belongsTo('App\Category');
+    }
+
+    /**
+     * Get the post's image.
+     */
+    public function image()
+    {
+        return $this->morphOne(MenuImage::class, 'imageable');
+    }
+
+    /**
+     * Build Image URL
+     *
+     * @return string
+     */
+    public function imageUrl() : string
+    {
+        return $this->image ? $this->buildUrl($this->image) : '';
+    }
+
+    /**
+     * Build Thumbnail URL
+     *
+     * @return string
+     */
+    public function thumbnailUrl() : string
+    {
+        return $this->image ? $this->buildUrl($this->image, true) : '';
+    }
+
+    /**
+     * Helper function for building URLs
+     *
+     * @param MenuImage $image
+     * @param bool      $thumbnail
+     * @return string
+     */
+    public function buildUrl(MenuImage $image, $thumbnail = false)
+    {
+        $path = config('menu_images.storage.food.local_path');
+        $filename = $thumbnail ? config('menu_images.thumbnail.prefix') . $image->filename : $image->filename;
+        $path .= DIRECTORY_SEPARATOR . $this->image->uuid . DIRECTORY_SEPARATOR . $filename;
+
+        return asset(Storage::url($path));
     }
 
     /**
