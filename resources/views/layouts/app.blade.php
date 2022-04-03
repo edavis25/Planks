9b24a036593f0b6a93b6b0e72b08fe93009f6c93
Eric Davis <davis.25811@gmail.com>
2022-04-03 00:57:30 -0400

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Planks') }}</title>
+    <link rel="shortcut icon" type="image/jpg" href="/img/favicon-32x32.png"/>
 
     <!-- Bootstrap 4 -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
